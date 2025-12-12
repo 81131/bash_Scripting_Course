8e8dc0dcb7e2f8b8161e9d24264881d0eadf1e67
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-12 09:17:18 +0530

    Added test operation part I

---
+++ Test Operators/simpleStringComparison.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "Instructions \nYou will get prompt to enter few words. \nIf the words match: 0\nIf the words doesn't match: 1\n\n"
+echo "Enter the first word: "
+read FIRSTWORD
+echo "Enter the second word: "
+read SECONDWORD
+
+test "$FIRSTWORD" = "$SECONDWORD"
+echo $?
