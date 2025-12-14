f58d9bcafb6c06998e363031b973b7bbf55ac49f
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-14 23:21:22 +0530

    Added functions example

---
+++ Functions/firstFunc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+showupTime(){
+	local up=$(uptime -p | cut -c4-)
+	local since=$(uptime -s)
+	cat << EOF
+
+-----------------------------------
+This machine has been up for ${up}
+It has been running since ${since}
+-----------------------------------
+EOF
+}
+
+showupTime
